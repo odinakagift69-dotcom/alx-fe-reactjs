d2dbb3e4b72c131357aecda7e7b0da809824f19a
Gift <odinakagift69@gmail.com>
2025-11-16 22:31:28 +0200

    Initial commit :Setup Recipe Sharing App with Zustand

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import './App.css'; // You can keep this if you want styles
+
+function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;

919598606c6f6be319bc497be50aea005f979d8a
Gift <odinakagift69@gmail.com>
2025-11-17 00:11:50 +0200

    Add recipe management components

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,5 @@
-// src/components/RecipeList.jsx
-import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';
+import { useRecipeStore } from '../store/recipeStore';
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
@@ -8,8 +8,7 @@ const RecipeList = () => {
     <div>
       {recipes.map((recipe) => (
         <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
+          <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
         </div>
       ))}
     </div>
@@ -17,3 +16,4 @@ const RecipeList = () => {
 };
 
 export default RecipeList;
+
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,8 +1,24 @@
-// src/components/recipeStore.js
-import create from 'zustand';
+// src/store/recipeStore.js
+import { create } from 'zustand';
+import { nanoid } from 'nanoid';
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
-  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes }),
+
+  addRecipe: (title, description) =>
+    set((state) => ({
+      recipes: [...state.recipes, { id: nanoid(), title, description }],
+    })),
+
+  updateRecipe: (id, updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+      ),
+    })),
+
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
 }));

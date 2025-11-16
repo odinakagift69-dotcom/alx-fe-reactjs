b81170d515ce39ccda611f51b431f3fac73976ad
Gift <odinakagift69@gmail.com>
2025-11-17 00:04:10 +0200

    Add recipe store and recipe components

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,8 @@
+// src/components/recipeStore.js
+import create from 'zustand';
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes }),
+}));

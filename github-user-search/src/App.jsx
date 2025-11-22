915b8d327bbe2a8319956ef827f5d9ef67de1b5a
Gift <odinakagift69@gmail.com>
2025-11-22 09:34:12 +0200

    Simplyify App.jsx for ALX Task 0

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,23 +1,10 @@
-import React, { useState } from "react";
-import SearchBar from "./components/SearchBar";
-import UserCard from "./components/UserCard";
-
 function App() {
-  const [users, setUsers] = useState([]);
-
   return (
-    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
+    <div>
       <h1>GitHub User Search</h1>
-      <SearchBar setUsers={setUsers} />
-      <div style={{ marginTop: "2rem" }}>
-        {users.length > 0 ? (
-          users.map((user) => <UserCard key={user.id} user={user} />)
-        ) : (
-          <p>No users yet. Try searching!</p>
-        )}
-      </div>
     </div>
   );
 }
 
 export default App;
+

67290d99bf5fbaf5a701fd403ea18b547c46023f
Gift <odinakagift69@gmail.com>
2025-11-22 10:23:02 +0200

    Fix React version and App.jsx for ALX Task 0

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,11 +1,13 @@
+import React from "react";
+
 function App() {
   return (
     <div>
       <h1>GitHub User Search</h1>
-      <p>Basic setup complete.</p>
     </div>
   );
 }
 
 export default App;
+
 

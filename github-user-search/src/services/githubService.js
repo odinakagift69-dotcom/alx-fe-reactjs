b284e5b30b902d3983837e59d58b86fc0e4d8513
Gift <odinakagift69@gmail.com>
2025-11-24 13:24:21 +0200

    Fix githubService.js to meet checker requirements

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,15 +1,11 @@
-export async function searchUsers({ username, location, minRepos, page }) {
-  let query = "";
+import axios from "axios";
 
-  if (username) query += `${username} `;
-  if (location) query += `location:${location} `;
-  if (minRepos) query += `repos:>${minRepos}`;
-
-  const url = `https://api.github.com/search/users?q=${encodeURIComponent(
-    query
-  )}&page=${page}&per_page=10`;
-
-  const response = await fetch(url);
-  return response.json();
-}
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 

a8aeaf8c5fb98597eb21e619ea126a979d199118
Gift <odinakagift69@gmail.com>
2025-11-29 22:44:04 +0200

    Added RecommendationsList.jsx to pass ALX checks

---
+++ recipe-sharing-app/src/components/RecommendationsList.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from "react";
+import useRecipeStore from "./recipeStore";
+import { Link } from "react-router-dom";
+
+const RecommendationsList = () => {
+  const recommendations = useRecipeStore((state) => state.recommendations);
+  const generateRecommendations = useRecipeStore(
+    (state) => state.generateRecommendations
+  );
+
+  useEffect(() => {
+    generateRecommendations();
+  }, [generateRecommendations]);
+
+  return (
+    <div>
+      <h2>Recommended For You</h2>
+
+      {recommendations.length === 0 && <p>No recommendations yet.</p>}
+
+      {recommendations.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+          </h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecommendationsList;
+
